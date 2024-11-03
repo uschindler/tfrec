0f2ff2a739e3d1efd047a210f65291702b153177
Uwe Schindler <uwe@thetaphi.de>
2024-11-03 18:41:28 +0100

    add timestamp to mqtt message

--- contrib/mqtt-publish.sh
+++ contrib/mqtt-publish.sh
@@ -4,8 +4,9 @@ temp=$2
 hum=$3
 lowbatt=$5
 rssi=$6
+ts=$8
 
 topic="tfa-weatherhub/$id"
-message="$temp $hum $lowbatt $rssi"
+message="$temp $hum $lowbatt $rssi $ts"
 
 exec /usr/bin/mosquitto_pub -u home -P lolifu -t "$topic" -r -m "$message"
